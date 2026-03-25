7652ff5a927cff05eb04abc8266af8616d764741
hz6n779rj4-debug <hz6n779rj4@privaterelay.appleid.com>
2026-03-26 00:53:12 +0100

    Update page.tsx

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,8 +1,1 @@
-export default function AdminPage() {
-  return (
-    <main style={{ padding: '24px', color: 'white' }}>
-      <h1>Admin</h1>
-      <p>Admin page temporary fix.</p>
-    </main>
-  )
-}
+export {}
